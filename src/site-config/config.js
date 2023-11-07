6816013c553e743e125cbcd0e10144b4f60f7df7
zycoJamie <410752387@qq.com>
2023-11-07 23:38:18 +0800

    WIP: Markdown render style

--- src/site-config/config.js
+++ src/site-config/config.js
@@ -1,8 +1,14 @@
+import mdSupPlugin from "markdown-it-sup";
+import mdSubPlugin from "markdown-it-sub";
+
 export const baseConfig = {
   cleanUrls: true,
   markdown: {
     theme: "github-light",
     lineNumbers: true,
     html: true,
+    config(md) {
+      md.use(mdSupPlugin).use(mdSubPlugin);
+    },
   },
 };
