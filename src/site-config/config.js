663cce0554ba7ed20ed28fb6b7e65da119a46ee2
zycoJamie <410752387@qq.com>
2024-02-05 23:33:33 +0800

    fix: Symbol is duplicated because of vite's optimizeDeps

--- src/site-config/config.js
+++ src/site-config/config.js
@@ -1,6 +1,15 @@
 import mdSupPlugin from "markdown-it-sup";
 import mdSubPlugin from "markdown-it-sub";
 import blogArchivePlugin from "../plugins/blogArchivePlugin.js";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const packagePath = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "../../package.json"
+);
+const PackageJson = JSON.parse(fs.readFileSync(packagePath));
 
 export const baseConfig = {
   cleanUrls: true,
@@ -14,5 +23,12 @@ export const baseConfig = {
   },
   vite: {
     plugins: [blogArchivePlugin({ postDir: "/posts" })],
+    optimizeDeps: {
+      /**
+       * 开发模式下，Symbol变量被提取进入单独模块，导致存在两个同名的Symbol变量
+       * 详情：https://github.com/vuejs/vitepress/issues/3292
+       */
+      exclude: [PackageJson.name],
+    },
   },
 };
