ab9da992c917438aceee8225ac04833bb2a99710
zycoJamie <410752387@qq.com>
2024-10-24 23:46:55 +0800

    fix: Hydration mismatch in prod

--- src/plugins/mdItMermaidPlugin.js
+++ src/plugins/mdItMermaidPlugin.js
@@ -15,21 +15,29 @@ const markdownItForMermaid = (mdIt) => {
       langAttrs = arr.slice(2).join("");
     }
     if (langName === "mermaid") {
+      /**
+       * 使用ClientOnly包裹的原因
+       * 在生产环境下，vitepress调用vue的ssr方法renderToString，这会将标签内的文本转义，导致两个问题
+       * 1. script内的js出现语法错误，不能执行
+       * 2. hydration mismatch
+       */
       return `<pre class='mermaid-container' style="visibility:hidden;">${tokens[idx].content}</pre>
-      <component is="script">
-      (() => {
-        const timer = setInterval(async () => {
-          if (window.mermaid) {
-            await window.mermaid.run({ querySelector: ".mermaid-container" });
-            const container = document.querySelector(".mermaid-container");
-            if (container) {
-              container.style.visibility = "visible";
+      <ClientOnly>
+        <component is="script">
+        (() => {
+          const timer = setInterval(async () => {
+            if (window.mermaid) {
+              await window.mermaid.run({ querySelector: ".mermaid-container" });
+              const container = document.querySelector(".mermaid-container");
+              if (container) {
+                container.style.visibility = "visible";
+              }
+              clearInterval(timer);
             }
-            clearInterval(timer);
-          }
-        }, 500);
-      })();
-      </component>`;
+          }, 500);
+        })();
+        </component>
+      </ClientOnly>`;
     }
     return defaultHandler(tokens, idx, options, env, self);
   };
